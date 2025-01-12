fef6b3c5cb6dde90e8ca410231d18df57d73c64e
Qcompiler <chenyidong@tsinghua.edu.cn>
2025-01-12 22:36:19 +0800

    update sm90

--- vllm/model_executor/layers/quantization/mixq.py
+++ vllm/model_executor/layers/quantization/mixq.py
@@ -10,6 +10,11 @@ from vllm.model_executor.layers.quantization.base_config import (
 from vllm.model_executor.utils import set_weight_attrs
 # from mixlib import w8_a16_gemm_forward_torch
 from EETQ import w8_a16_gemm 
+
+arch = torch.cuda.get_device_capability()
+arch = arch[0] * 10 + arch[1] 
+if arch >= 90:
+    import mixgemm
 def FindOutliers(Activation, sigma = None):
 
     if sigma is None:
@@ -28,6 +33,7 @@ class MixQConfig(QuantizationConfig):
         weight_bits: int
     ) -> None:
         self.weight_bits = weight_bits
+        self.pack_factor = 8
         # self.group_size = group_size
         
 
@@ -82,6 +88,7 @@ class MixQLinearMethod(LinearMethodBase):
     def __init__(self, quant_config: MixQConfig, weight_only = False):
         self.quant_config = quant_config
         self.weight_only = weight_only
+        self.use_exact = False
         
 
 
@@ -121,20 +128,59 @@ class MixQLinearMethod(LinearMethodBase):
         layer.register_parameter("weight", weight)
         set_weight_attrs(weight, extra_weight_attrs)
 
-
-        q_weight = Parameter(
+        if arch >= 90:
+            q_weight = Parameter(
             torch.empty(
-                input_size_per_partition,
-                output_size_per_partition,
-                dtype=torch.int8,
+                output_size_per_partition ,
+                input_size_per_partition // self.quant_config.pack_factor,
+                dtype=torch.int32,
             ),
             requires_grad=False,
-        )
-        set_weight_attrs(
-            q_weight, {
+            )
+            set_weight_attrs(
+                q_weight, {
+                    "input_dim": 1,
+                    "output_dim": 0,
+                    "packed_dim": 1,
+                    "pack_factor": self.quant_config.pack_factor,
+                })
+            q_scale_col = Parameter(
+            torch.empty(
+                1,
+                output_size_per_partition,
+                dtype=params_dtype,
+            ),
+            requires_grad=False,
+            )
+            set_weight_attrs(q_scale_col, {
                 "input_dim": 0,
                 "output_dim": 1,
-                 
+            })
+
+        else:
+            q_weight = Parameter(
+                torch.empty(
+                    input_size_per_partition,
+                    output_size_per_partition,
+                    dtype=torch.int8,
+                ),
+                requires_grad=False,
+            )
+            set_weight_attrs(
+                q_weight, {
+                    "input_dim": 0,
+                    "output_dim": 1,
+                    
+                })
+            q_scale_col = Parameter(
+                torch.empty(
+                    output_size_per_partition,
+                    dtype=params_dtype,
+                ),
+                requires_grad=False,
+            )
+            set_weight_attrs(q_scale_col, {
+                "output_dim": 0,
             })
         layer.register_parameter("q_weight", q_weight)
         set_weight_attrs(q_weight, extra_weight_attrs)
@@ -171,16 +217,7 @@ class MixQLinearMethod(LinearMethodBase):
 
 
 
-        q_scale_col = Parameter(
-            torch.empty(
-                output_size_per_partition,
-                dtype=params_dtype,
-            ),
-            requires_grad=False,
-        )
-        set_weight_attrs(q_scale_col, {
-            "output_dim": 0,
-        })
+
         layer.register_parameter("q_scale_col", q_scale_col)
         set_weight_attrs(q_scale_col, extra_weight_attrs)
 
@@ -200,13 +237,15 @@ class MixQLinearMethod(LinearMethodBase):
         set_weight_attrs(scale_col, extra_weight_attrs)
         layer.capture = 0
         layer.n_outliers = 128
-        
-    def apply(self,
-              layer: torch.nn.Module,
-              x: torch.Tensor,
-              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        if arch >= 90:
+            layer.weight_cache2 = torch.clone(weight_cache.data)
+            layer.out = torch.zeros((1, output_size_per_partition), dtype=torch.half, device=weight_cache.data.device)
+            layer.q_scale_col_f32  = torch.zeros((layer.q_scale_col.shape), dtype=torch.float16,
+                                                  device=weight_cache.data.device)
+                
         
 
+    def apply_sm89(self, layer, x, bias):
         shape = x.shape[:-1] + (layer.weight.shape[0], )
 
 
@@ -214,18 +253,7 @@ class MixQLinearMethod(LinearMethodBase):
         M =  inputs.shape[0]
         N = layer.weight.shape[0]
         K = layer.weight.shape[1]
-        # print("call 8 bit mix")
-        # print(x.shape,end="")
-        # print(layer.weight.shape)
 
-        # if  torch.isnan(torch.abs(torch.sum(inputs[0:1024]))) :
-        #     print(x.shape)
-        #     print(layer.weight.shape)
-        #     print("torch.isnan(torch.abs(torch.sum(inputs[0:1024])))")
-        #     print(torch.sum(inputs[0:1024]))
-                
-
-        #     exit()
             
         apply_capture = True
         if not layer.capture and apply_capture:
@@ -236,24 +264,11 @@ class MixQLinearMethod(LinearMethodBase):
             
             layer.n_outliers = len(local_ind)      
     
-            # if  layer.n_outliers:
-            #     layer.ind.data = local_ind
-            #     layer.weight_cache.data = layer.weight.data[:, local_ind] *  layer.scale_col.reshape((N, 1))
-            #     layer.weight_cache.data = layer.weight_cache.data.to(torch.float16)
-
         if    self.weight_only is True:
 
             # y1 =  w8_a16_gemm(inputs,layer.q_weight,layer.q_scale_col)
             y1 = w8_a16_gemm(inputs,layer.q_weight,layer.q_scale_col)
-            # print('-------------')
-            # print(inputs)
-            # print(layer.q_weight)
-            # print(layer.q_scale_col)
-            # print(y1)
-            # print('-------------')
-            # if self.weight_only is True:
-
-            #     exit()
+      
         else:
             # for compute bound 
             if M > 32:
@@ -288,39 +303,95 @@ class MixQLinearMethod(LinearMethodBase):
 
         if layer.bias is not None:
             y1 += layer.bias
-
-        # if  torch.isnan(torch.abs(torch.sum(x[0:1024]))) :
-        #     print("sum == 0")
-        #     print(x)
-                
-                
-
-        #     print(y1[0:3,0:3])
-        #     exit()
-        # #print(self.ind.shape[0])
-        #print("grand is ")
-        #print(y1[0:3,0:3])
-        #exit()
-        # # print("EETQ is ")
-        # # print(y2)
         
-        # err = torch.norm(y1 - y2)
-        # print(layer.name)
-        # if   layer.q_weight.shape[1] == 11008*2:
-        #     #torch.save(layer.q_weight,"eetq.pt")
-        #     #torch.save(inputs,"activation.pt")
-        #     #torch.save(layer.q_scale_col,"q_scale_col.pt")
-        #     #print(layer.weight.shape)
-            
-        #     #print(layer.q_weight.shape)
-        #     print(layer.q_weight)
-        #     #print(layer.q_weight[1,0:4096])
-        #     print(err)
-        #     exit(0)
-            
-        #exit(0)
-        # if M < 1024:
-        #     print(y1)
         return y1.reshape(shape)
         
+
+    def apply_sm90(self, layer, x, bias):
+
+        shape = x.shape[:-1] + (layer.weight.shape[0], )
+
+
+        inputs = x.reshape(-1, x.shape[-1])
+        M =  inputs.shape[0]
+        N = layer.weight.shape[0]
+        K = layer.weight.shape[1]
+
+            
+        apply_capture = True
+        if not layer.capture and apply_capture:
+
+
+            layer.capture = 1
+            local_ind = FindOutliers(inputs)
+            
+            layer.n_outliers = len(local_ind)
+            layer.weight_cache2 = layer.weight_cache.data / layer.q_scale_col.T
+            layer.q_scale_col_f32  = layer.q_scale_col.data.to(torch.float32)
+
+            if  self.weight_only is True and self.use_exact:
+                layer.weight.data = layer.weight.to(torch.float16) * layer.scale_col.T
+
+        if   self.weight_only is True and self.use_exact:
+
+
+            
+            y1 = torch.mm(inputs, layer.weight.T)
+
+        else:
+            if M == 1:
+                mixgemm.gemv_int4_fp16_mix(M, N, K, inputs, layer.q_weight,
+                                   layer.out, 64, 4, 
+                                   layer.q_scale_col_f32,
+                                   layer.weight_cache2, layer.ind, 
+                                   128)
+           
+                if bias is not None:
+                    layer.out.add_(bias)
+                return layer.out.reshape(shape)
+
+
+            else:
+            
+                if layer.n_outliers == 0:
+
+                    
+                    y1 = mixlib.mixgemmforward_direct(M,N,K,
+                                        x,
+                                        layer.weight, 
+                                        layer.scale_col)
+
+
+                else:
+
+                    #print("call  weight act",M,N,K)
+                    y1 = mixlib.mixgemmforward(M,N,K,
+                                        x,
+                                        layer.weight, 
+                                        layer.scale_col,
+                                        layer.weight_cache, #新增fp weight 
+                                        layer.ind, #新增fp ind 
+                                        layer.q_weight, #新增 int4 weight int32
+                                        layer.q_scale_col)
+                 
+       
+
+
+            
+            
+
+        if layer.bias is not None:
+            y1 += layer.bias
         
+        return y1.reshape(shape)        
+
+    def apply(self,
+              layer: torch.nn.Module,
+              x: torch.Tensor,
+              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        
+        if arch <= 89:
+            return self.apply_sm89(layer, x, bias)
+        else:
+            return self.apply_sm90(layer, x, bias)
+                
